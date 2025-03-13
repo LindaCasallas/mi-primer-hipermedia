70ca93f4e6f808df1897ca48bcbc6bef79976aad
Linda Casallas <lmcasallasa@libertadores.edu.co>
2025-03-13 02:16:52 +0000

    Boton de Snoopy piloto

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-document.getElementById("Haz click para ver a Snoopy").addEventListener("click", function() {
-    var img = document.getElementById("piloto.png");
-    img.style.display = (img.style.display === "none") ? "block" : "none";
+document.getElementById("snoopybutton").addEventListener("click", function() {
+    var img = document.getElementById("piloto");
+    img.style.display = "block";
+    img.style.opacity = 1;
+
+    var posX = 0;
+    var posY = 0;
+    var directionX = 1;
+    var directionY = 1;
+    var speed = 2;
+
+    function moveImage() {
+        var imgWidth = img.offsetWidth;
+        var imgHeight = img.offsetHeight;
+        var windowWidth = window.innerWidth;
+        var windowHeight = window.innerHeight;
+
+        posX += directionX * speed;
+        posY += directionY * speed;
+
+        if (posX + imgWidth > windowWidth || posX < 0) {
+            directionX *= -1;
+        }
+        if (posY + imgHeight > windowHeight || posY < 0) {
+            directionY *= -1;
+        }
+
+        img.style.left = posX + "px";
+        img.style.top = posY + "px";
+
+        requestAnimationFrame(moveImage);
+    }
+
+    moveImage();
 });
