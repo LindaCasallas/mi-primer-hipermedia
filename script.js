b4a2542b0a8cfaf6ab2d33970b01b71f9cf1abe9
Linda Casallas <lmcasallasa@libertadores.edu.co>
2025-03-12 18:31:23 -0700

    Create script.js
    creacion de script y agregar la funcionalidad

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("Haz click para ver a Snoopy").addEventListener("click", function() {
+    var img = document.getElementById("piloto.png");
+    img.style.display = (img.style.display === "none") ? "block" : "none";
+});
